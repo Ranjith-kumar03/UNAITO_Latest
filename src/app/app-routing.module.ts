4f14fb4f45016eab954f0d0546bd0533e8d15884
unknown <ranjithkumar03.1980@gmail.com>
2022-06-05 18:47:55 +0530

    Card size for administration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,37 +28,39 @@ import { DatabaseServerComponent } from './Component/database-server/database-se
 import { AddStorageComponent } from './Component/add-storage/add-storage.component';
 import { SecurityComplianceComponent } from './Component/security-compliance/security-compliance.component';
 import { AddSecurCompiComponent } from './Component/add-secur-compi/add-secur-compi.component';
+import { AdministrationComponent } from './Component/administration/administration.component';
 
 
 const routes: Routes = [
- { path: '', pathMatch: 'full', redirectTo: 'login'},
-  {path:"login" , component:LoginComponent},
-  {path:"forgetpassword" , component:ForgetPasswordComponent},
-  {path:"application" , component:ApplicationComponent},
-  {path:"dashboard" , component:DashboardComponent},
-  {path:"projectlist",component:ProjectListComponent},
-  {path:"newproject",component:CreateNewProjectComponent},
-  {path:"projectdetailsDashboard/:id", component:ProjectDetailsDashboardComponent},
-  {path:"addDrivers", component:ProjectDetailsDriversComponent},
-  {path:"editDrivers", component:EditDriversComponent},
-  {path:"addScope", component:ProjectDetailsScopeComponent},
-  {path:"overview", component:ProjectOverViewComponent},
-  {path:"teamonboard", component:TeamOnboardingComponent},
-  {path:"assessmentprojectplan", component:AssessmentProjectplanComponent},
-  {path:"addAppOverview", component:AddApplicationOverviewComponent},
-  {path:"addAppOverviewdetails", component:AddApplicationOverviewDetailsComponent},
-  {path:"server", component:ServersComponent},
-  {path:"addWebServer", component:AddWebserverComponent},
-  {path:"addappServer", component:ApllicationServerComponent},
-  {path:'adddatbaseserver',component:DatabaseServerComponent},
-  {path:"storage", component:StorageComponent},
-  {path:"addstorage", component:AddStorageComponent},
-  {path:"applicationLayer", component:ApplicationLayerComponent},
-  {path:"addApplicationLayerDetails", component:AddApplicationLayerComponent},
-  {path:"addIntegration", component:IntegrationsComponent},
-  {path:"securityCompliance", component:SecurityComplianceComponent},
-  {path:"addSecurCompi",component:AddSecurCompiComponent},
-  {path:"logout" , component:LogoutComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: "login", component: LoginComponent },
+  { path: "forgetpassword", component: ForgetPasswordComponent },
+  { path: "application", component: ApplicationComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "projectlist", component: ProjectListComponent },
+  { path: "newproject", component: CreateNewProjectComponent },
+  { path: "projectdetailsDashboard/:id", component: ProjectDetailsDashboardComponent },
+  { path: "addDrivers", component: ProjectDetailsDriversComponent },
+  { path: "editDrivers", component: EditDriversComponent },
+  { path: "addScope", component: ProjectDetailsScopeComponent },
+  { path: "overview", component: ProjectOverViewComponent },
+  { path: "teamonboard", component: TeamOnboardingComponent },
+  { path: "assessmentprojectplan", component: AssessmentProjectplanComponent },
+  { path: "addAppOverview", component: AddApplicationOverviewComponent },
+  { path: "addAppOverviewdetails", component: AddApplicationOverviewDetailsComponent },
+  { path: "server", component: ServersComponent },
+  { path: "addWebServer", component: AddWebserverComponent },
+  { path: "addappServer", component: ApllicationServerComponent },
+  { path: 'adddatbaseserver', component: DatabaseServerComponent },
+  { path: "storage", component: StorageComponent },
+  { path: "addstorage", component: AddStorageComponent },
+  { path: "applicationLayer", component: ApplicationLayerComponent },
+  { path: "addApplicationLayerDetails", component: AddApplicationLayerComponent },
+  { path: "addIntegration", component: IntegrationsComponent },
+  { path: "securityCompliance", component: SecurityComplianceComponent },
+  { path: "addSecurCompi", component: AddSecurCompiComponent },
+  { path: "administration", component: AdministrationComponent },
+  { path: "logout", component: LogoutComponent }
 ];
 
 @NgModule({
