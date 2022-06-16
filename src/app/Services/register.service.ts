556177216cb46d5afc86baccd382abbc3dfb5d47
unknown <ranjithkumar03.1980@gmail.com>
2022-06-16 19:56:41 +0530

    createuserLogicadded

---
+++ src/app/Services/register.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { RegisterUser } from '../Component/create-new-user/create-new-user.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  constructor(private http: HttpClient) { }
+
+  create(userName: String, firstName:String,lastName:String,email:String,contactNumber:String,location:String,roleName:String) {
+    return this.http.post(environment.API_REGISTER_URL,{userName,firstName,lastName,email,contactNumber,location,roleName},{headers:new HttpHeaders({
+      "API-KEY":"USER-API-KEY"
+    })});
+}
+
+update(updateUser: RegisterUser) {
+    return this.http.put(environment.API_REGISTER_URL ,updateUser);
+}
+}
